cc6b5dd1acd0eb57672a94d78c8582eb064c57a7
Tee Tanawatanakul <wallsfantasy@gmail.com>
2019-01-16 00:18:17 +0700

    Finished lecture 18

---
+++ src/main/java/com/example/restdemo/user/UserResource.java
@@ -0,0 +1,25 @@
+package com.example.restdemo.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    @Autowired
+    private UserDaoService service;
+
+    @GetMapping(path = "/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping(path = "/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        return service.findOne(id);
+    }
+}
